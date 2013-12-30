8fc837dc17bc9ee0d750205dbf5c7e298c66720b
Viacheslav Lotsmanov (unclechu) <lotsmanov89@gmail.com>
2013-12-30 04:29:53 +0400

    Check for num of channels in jack_process

--- src/jacktoalsa.c
+++ src/jacktoalsa.c
@@ -9,6 +9,7 @@
  *   set hardware params for custom alsa card
  *   custom num of outs and ins channels
  *   watch for system:playback and system:capture and forward to alsa
+ *   32 and 24 bit depth
  */
 
 #include <stdlib.h>
@@ -19,18 +20,18 @@
 #include <math.h>
 
 // default: stereo
-short num_out_channels = 2; // jack-outputs, alsa-inputs
-short num_in_channels = 2; // jack-inputs, alsa-outputs
+short num_capture_channels = 2; // jack-outputs, alsa-inputs
+short num_playback_channels = 2; // jack-inputs, alsa-outputs
 
 // jack
 jack_client_t *jack_client = NULL;
 char jack_client_name[128] = "meta_jacktoalsa";
 uint32_t buffer_size = 0;
 uint32_t sample_rate = 0;
-jack_port_t **output_ports = NULL;
-jack_port_t **input_ports = NULL;
-jack_default_audio_sample_t **outputs_buf = NULL;
-jack_default_audio_sample_t **inputs_buf = NULL;
+jack_port_t **capture_ports = NULL;
+jack_port_t **playback_ports = NULL;
+jack_default_audio_sample_t **capture_buf = NULL;
+jack_default_audio_sample_t **playback_buf = NULL;
 
 // alsa
 snd_pcm_t *alsa_playback_handle = NULL;
@@ -38,8 +39,8 @@ snd_pcm_t *alsa_capture_handle = NULL;
 char alsa_card_playback[128] = "default";
 char alsa_card_capture[128] = "default";
 snd_pcm_format_t alsa_bit_depth = SND_PCM_FORMAT_S16;
-int16_t *inbuf16bit = NULL;
-int16_t *outbuf16bit = NULL;
+int16_t *playback_buf16bit = NULL;
+int16_t *capture_buf16bit = NULL;
 bool alsa_init = false;
 
 inline int16_t float_to_int16(float v) {
@@ -63,45 +64,46 @@ int jack_process(jack_nframes_t nframes, void *arg) {
     int res;
     int channel, bufval, n;
 
-    // prepare ports to read and write
-
-    for (channel=0; channel<num_out_channels; channel++) {
-        outputs_buf[channel] = (jack_default_audio_sample_t *)
-            jack_port_get_buffer(output_ports[channel], nframes);
-    }
-
-    for (channel=0; channel<num_in_channels; channel++) {
-        inputs_buf[channel] = (jack_default_audio_sample_t *)
-            jack_port_get_buffer(input_ports[channel], nframes);
-    }
-
     // get from jack-input and write to alsa-playback
+    if (num_playback_channels > 0) {
+        for (channel=0; channel<num_playback_channels; channel++) {
+            playback_buf[channel] = (jack_default_audio_sample_t *)
+                jack_port_get_buffer(playback_ports[channel], nframes);
+        }
 
-    if (alsa_bit_depth == SND_PCM_FORMAT_S16) {
-        for ( bufval = 0, n = 0;
-              bufval < (nframes * num_in_channels);
-              bufval = (bufval + num_in_channels), n++ )
-            for (channel=0; channel<num_in_channels; channel++)
-                inbuf16bit[bufval + channel] = float_to_int16(inputs_buf[channel][n]);
-    }
+        if (alsa_bit_depth == SND_PCM_FORMAT_S16) {
+            for ( bufval = 0, n = 0;
+                  bufval < (nframes * num_playback_channels);
+                  bufval = (bufval + num_playback_channels), n++ )
+                for (channel=0; channel<num_playback_channels; channel++)
+                    playback_buf16bit[bufval + channel] = float_to_int16(playback_buf[channel][n]);
+        }
 
-    res = snd_pcm_writei(alsa_playback_handle, inbuf16bit, nframes);
-    if (res == -EPIPE) { // heal the overruns
-        res = snd_pcm_recover(alsa_playback_handle, res, 1);
-        if (res >= 0)
-            res = snd_pcm_writei(alsa_playback_handle, inbuf16bit, nframes);
+        res = snd_pcm_writei(alsa_playback_handle, playback_buf16bit, nframes);
+        if (res == -EPIPE) { // heal the overruns
+            res = snd_pcm_recover(alsa_playback_handle, res, 1);
+            if (res >= 0)
+                res = snd_pcm_writei(alsa_playback_handle, playback_buf16bit, nframes);
+        }
     }
 
     // get from alsa-capture and write to jack-output
+    if (num_capture_channels > 0) {
+        for (channel=0; channel<num_capture_channels; channel++) {
+            capture_buf[channel] = (jack_default_audio_sample_t *)
+                jack_port_get_buffer(capture_ports[channel], nframes);
+        }
 
-    res = snd_pcm_readi(alsa_capture_handle, outbuf16bit, nframes);
-    if (res == -EPIPE) snd_pcm_prepare(alsa_capture_handle); // heal the overruns
-    if (alsa_bit_depth == SND_PCM_FORMAT_S16) {
-        for ( bufval = 0, n = 0;
-              bufval < (nframes * num_out_channels);
-              bufval = (bufval + num_out_channels), n++ )
-            for (channel=0; channel<num_out_channels; channel++)
-                outputs_buf[channel][n] = int16_to_float(outbuf16bit[bufval + channel]);
+        res = snd_pcm_readi(alsa_capture_handle, capture_buf16bit, nframes);
+        if (res == -EPIPE) snd_pcm_prepare(alsa_capture_handle); // heal the overruns
+
+        if (alsa_bit_depth == SND_PCM_FORMAT_S16) {
+            for ( bufval = 0, n = 0;
+                  bufval < (nframes * num_capture_channels);
+                  bufval = (bufval + num_capture_channels), n++ )
+                for (channel=0; channel<num_capture_channels; channel++)
+                    capture_buf[channel][n] = int16_to_float(capture_buf16bit[bufval + channel]);
+        }
     }
 
     return 0;
@@ -113,11 +115,11 @@ int jack_new_buffer(jack_nframes_t nframes, void *arg) {
 
     fprintf(stdout, "ALSA: reallocate memory for buffer\n");
     if (alsa_bit_depth == SND_PCM_FORMAT_S16) {
-        if (inbuf16bit != NULL) free(inbuf16bit);
-        inbuf16bit = malloc( (nframes * sizeof(short)) * num_in_channels );
+        if (playback_buf16bit != NULL) free(playback_buf16bit);
+        playback_buf16bit = malloc( (nframes * sizeof(short)) * num_playback_channels );
 
-        if (outbuf16bit != NULL) free(outbuf16bit);
-        outbuf16bit = malloc( (nframes * sizeof(short)) * num_out_channels );
+        if (capture_buf16bit != NULL) free(capture_buf16bit);
+        capture_buf16bit = malloc( (nframes * sizeof(short)) * num_capture_channels );
     } else {
         fprintf(stderr, "Unsupported bit depth of ALSA (in jack_new_buffer)\n");
         exit(EXIT_FAILURE);
@@ -155,36 +157,36 @@ int init_jack() {
         return 1;
     }
 
-    fprintf(stdout, "JACK: registering output ports\n");
-    output_ports = malloc(sizeof(jack_port_t*) * num_out_channels);
-    outputs_buf = malloc(sizeof(jack_default_audio_sample_t*) * num_out_channels);
-    for (i=0; i<num_out_channels; i++) {
+    fprintf(stdout, "JACK: registering output (capture) ports\n");
+    capture_ports = malloc(sizeof(jack_port_t*) * num_capture_channels);
+    capture_buf = malloc(sizeof(jack_default_audio_sample_t*) * num_capture_channels);
+    for (i=0; i<num_capture_channels; i++) {
         sprintf(port_name, "capture_%d", i+1);
-        output_ports[i] = jack_port_register( jack_client, 
-                                              port_name,
-                                              JACK_DEFAULT_AUDIO_TYPE,
-                                              JackPortIsOutput | JackPortIsPhysical,
-                                              0 );
-        if (output_ports[i] != NULL) {
-            fprintf(stdout, "JACK: output port \"%s\" registered\n", port_name);
+        capture_ports[i] = jack_port_register( jack_client, 
+                                               port_name,
+                                               JACK_DEFAULT_AUDIO_TYPE,
+                                               JackPortIsOutput | JackPortIsPhysical,
+                                               0 );
+        if (capture_ports[i] != NULL) {
+            fprintf(stdout, "JACK: output (capture) port \"%s\" registered\n", port_name);
         } else {
             fprintf(stderr, "JACK: no more ports available\n");
             return 1;
         }
     }
 
-    fprintf(stdout, "JACK: registering input ports\n");
-    input_ports = malloc(sizeof(jack_port_t*) * num_in_channels);
-    inputs_buf = malloc(sizeof(jack_default_audio_sample_t*) * num_in_channels);
-    for (i=0; i<num_in_channels; i++) {
+    fprintf(stdout, "JACK: registering input (playback) ports\n");
+    playback_ports = malloc(sizeof(jack_port_t*) * num_playback_channels);
+    playback_buf = malloc(sizeof(jack_default_audio_sample_t*) * num_playback_channels);
+    for (i=0; i<num_playback_channels; i++) {
         sprintf(port_name, "playback_%d", i+1);
-        input_ports[i] = jack_port_register( jack_client, 
-                                             port_name,
-                                             JACK_DEFAULT_AUDIO_TYPE,
-                                             JackPortIsInput | JackPortIsPhysical,
-                                             0 );
-        if (input_ports[i] != NULL) {
-            fprintf(stdout, "JACK: input port \"%s\" registered\n", port_name);
+        playback_ports[i] = jack_port_register( jack_client, 
+                                                port_name,
+                                                JACK_DEFAULT_AUDIO_TYPE,
+                                                JackPortIsInput | JackPortIsPhysical,
+                                                0 );
+        if (playback_ports[i] != NULL) {
+            fprintf(stdout, "JACK: input (playback) port \"%s\" registered\n", port_name);
         } else {
             fprintf(stderr, "JACK: no more ports available\n");
             return 1;
@@ -239,7 +241,7 @@ int init_alsa() {
     fprintf(stdout, "ALSA: set playback parameters\n");
     res = snd_pcm_set_params( alsa_playback_handle, alsa_bit_depth,
                               SND_PCM_ACCESS_RW_INTERLEAVED,
-                              num_in_channels, sample_rate, 1, 0);
+                              num_playback_channels, sample_rate, 1, 0);
     if (res < 0)
         fprintf(stderr, "ALSA: cannot set playback parameters: %s\n", snd_strerror(res));
 
@@ -255,7 +257,7 @@ int init_alsa() {
     fprintf(stdout, "ALSA: set capture parameters\n");
     res = snd_pcm_set_params( alsa_capture_handle, alsa_bit_depth,
                               SND_PCM_ACCESS_RW_INTERLEAVED,
-                              num_out_channels, sample_rate, 1, 0);
+                              num_capture_channels, sample_rate, 1, 0);
     if (res < 0)
         fprintf(stderr, "ALSA: cannot set capture parameters: %s\n", snd_strerror(res));
 
